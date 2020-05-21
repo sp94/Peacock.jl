b251fe954bb1b2374f9ad9dc328d3ff8e06f7be1
Samuel Palmer <samuelpalmer94@gmail.com>
2020-05-21 11:00:25 +0100

    Include normalisation utils

--- src/utils.jl
+++ src/utils.jl
@@ -30,3 +30,32 @@ struct DiagonalMatrix <: AbstractMatrix{ComplexF64}
 end
 Base.size(A::DiagonalMatrix) = (length(A.diag), length(A.diag))
 Base.getindex(A::DiagonalMatrix, I::Vararg{Int,2}) = I[1]==I[2] ? A.diag[I[1]] : 0
+
+
+"""
+To do
+"""
+function normalise(data; weighting=I)
+    out = copy(data)
+    for n in 1:size(out,2)
+        out[:,n] /= sqrt(abs(dot(out[:,n],weighting*out[:,n])))
+    end
+    return out
+end
+
+
+"""
+To do
+"""
+function orthonormalise(data; weighting=I)
+    out = copy(data)
+    out[:,1] = normalise(out[:,1], weighting=weighting)
+    # Gram-Schmidt orthogonalisation
+    projector = zeros(size(out,1),size(out,1))
+    for n in 2:size(out,2)
+        projector += out[:,n-1]*out[:,n-1]' * weighting
+        out[:,n] = out[:,n] - projector*out[:,n]
+        out[:,n] = normalise(out[:,n], weighting=weighting)
+    end
+    return out
+end
