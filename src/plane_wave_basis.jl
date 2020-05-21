a482a1bb97d8478aac94d74bc924a19a994ef8d8
Samuel Palmer <samuelpalmer94@gmail.com>
2020-05-21 11:04:49 +0100

    Add HilbertSpace structure

--- src/plane_wave_basis.jl
+++ src/plane_wave_basis.jl
@@ -102,3 +102,67 @@ struct Mode <: AbstractVector{ComplexF64}
 end
 Base.size(A::Mode) = size(A.data)
 Base.getindex(A::Mode, I::Int) = A.data[I]
+
+
+"""
+Contains eigenvectors in the columns of data
+Is always orthonormalised
+"""
+struct HilbertSpace
+    k0::Vector{Float64}
+    data::Matrix{ComplexF64}
+    weighting::Matrix{ComplexF64}
+    basis::PlaneWaveBasis
+    function HilbertSpace(k0::Vector{Float64}, data::Matrix{ComplexF64},
+                            weighting::Matrix{ComplexF64}, basis::PlaneWaveBasis)
+        # Inner constructor guarantees data will be orthonormalised
+        data = orthonormalise(data, weighting=weighting)
+        return new(k0, data, weighting, basis)
+    end
+end
+
+
+"""
+To do
+"""
+function HilbertSpace(modes::Array{Mode,1})
+    k0 = modes[1].k0
+    data = zeros(ComplexF64, length(modes[1].data), length(modes))
+    weighting = modes[1].weighting
+    basis = modes[1].basis
+    for (col,mode) in enumerate(modes)
+        @assert mode.k0 == k0
+        @assert mode.weighting == weighting
+        @assert mode.basis == basis
+        data[:,col] = mode.data
+    end
+    return HilbertSpace(k0, data, weighting, basis)
+end
+
+
+"""
+To do
+Also: add test along the lines of:
+        hs = HilbertSpace(mode)
+        hs = Peacock.shift_k0(hs, 1, 2)
+        m = Mode(hs.k0, hs.data[:,1], hs.weighting, hs.basis, "")
+        @assert get_field(mode) ≈ get_field(m)
+"""
+function shift_k0(space::HilbertSpace, dp::Int, dq::Int)
+    data = space.data
+    ps = space.basis.ps
+    qs = space.basis.qs
+    data_new = zeros(ComplexF64, size(data))
+    for row in 1:size(data,1)
+        # Identify the corresponding (p_new,q_new) indices
+        # in the plane wave basis
+        p_new = ps[row] - dp
+        q_new = qs[row] - dq
+        row_new = findfirst(x->x==(p_new,q_new), collect(zip(ps,qs)))
+        if !isnothing(row_new)
+            data_new[row_new,:] = data[row,:]
+        end
+    end
+    k0_new = space.k0 + dp*space.basis.b1 + dq*space.basis.b2
+    return HilbertSpace(k0_new, data_new, space.weighting, space.basis)
+end
