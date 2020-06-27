14cbbc9784b415bd84fccac8486c0f69da06daac
S J Palmer <samuelpalmer94@gmail.com>
2020-06-27 21:39:38 +0100

    Give docs an explicit sidebar layout

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,18 @@
 using Documenter, Peacock
 
-makedocs(sitename="Peacock.jl Documentation")
+makedocs(
+    sitename="Peacock.jl Documentation",
+    pages = [
+        "index.md",
+        "Tutorials" => [
+            "tutorials/getting_started.md",
+        ],
+        "How-to guides" => [
+            "how-tos/zoo.md",
+        ],
+        "reference/index.md",
+    ]
+)
 
 deploydocs(
 	repo="github.com/sp94/Peacock.jl.git")
