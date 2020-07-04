f970bb7137e176c82615395e4ca3e929e49997f5
S J Palmer <samuelpalmer94@gmail.com>
2020-07-04 17:14:27 +0100

    docs: add missing docstrings to Zoo

--- src/Zoo.jl
+++ src/Zoo.jl
@@ -43,11 +43,18 @@ function wu_ep(x, y, angles, Rs, d1s, d2s, ep_bg, ep_cyl)
     return ep_bg
 end
 
-function make_wu_generic(Rs, d1s, d2s, polarisation, P; angles=[60n for n in 0:5], ep_bg=1, ep_cyl=11.7)
+"""
+    make_wu_generic(Rs, d1s, d2s, polarisation, cutoff;
+                angles=[60n for n in 0:5], ep_bg=1, ep_cyl=11.7)
+
+Returns a `NamedTuple` of `geometry`, `solver`, `polarisation`, and the `G`,
+`M`, `K` high-symmetry points for photonic crystals similar to Wu et al, 2015.
+"""
+function make_wu_generic(Rs, d1s, d2s, polarisation, cutoff; angles=[60n for n in 0:5], ep_bg=1, ep_cyl=11.7)
     epf(x,y) = wu_ep(x, y, angles, Rs, d1s, d2s, ep_bg, ep_cyl)
     muf(x,y) = 1.0
     geometry = Geometry(epf, muf, -60, +60, 1/500, 1/500)
-    solver = Solver(geometry, P)
+    solver = Solver(geometry, cutoff)
     G = BrillouinZoneCoordinate(  0,   0, "Γ")
     M = BrillouinZoneCoordinate(  0, 1/2, "M")
     K = BrillouinZoneCoordinate(1/3, 1/3, "K")
@@ -58,34 +65,58 @@ end
 
 
 """
-    make_wu(a0_div_R, P)
+    make_wu(a0_div_R, cutoff)
 
 Reproduces "Scheme for Achieving a Topological Photonic Crystal
-by Using Dielectric Material", Wu et al, 2015
+by Using Dielectric Material", Wu et al, 2015.
+
+Returns a `NamedTuple` of `geometry`, `solver`, `polarisation`, and the
+`G`, `M`, `K` high-symmetry points.
 """
-function make_wu(a0_div_R, P)
+function make_wu(a0_div_R, cutoff)
     # Geometry
     R = 1 / a0_div_R # assuming a0 = 1
     d = 2R/3
-    return make_wu_generic(R, d, d, TM, P)
+    return make_wu_generic(R, d, d, TM, cutoff)
 end
 
-function make_wu_triv(P::Int)
-    return make_wu(3.125, P)
+"""
+    make_wu_topo(cutoff::Int)
+
+Reproduces the trivial crystal (a0_div_R=3.125)from  "Scheme for Achieving a
+Topological Photonic Crystal by Using Dielectric Material", Wu et al, 2015.
+
+Returns a `NamedTuple` of `geometry`, `solver`, `polarisation`, and the
+`G`, `M`, `K` high-symmetry points.
+"""
+function make_wu_triv(cutoff::Int)
+    return make_wu(3.125, cutoff)
 end
 
-function make_wu_topo(P::Int)
-    return make_wu(2.9, P)
+"""
+    make_wu_topo(cutoff::Int)
+
+Reproduces the topological crystal (`a0_div_R=2.9`) from "Scheme for Achieving a
+Topological Photonic Crystal by Using Dielectric Material", Wu et al, 2015.
+
+Returns a `NamedTuple` of `geometry`, `solver`, `polarisation`, and the
+`G`, `M`, `K` high-symmetry points.
+"""
+function make_wu_topo(cutoff::Int)
+    return make_wu(2.9, cutoff)
 end
 
 
 """
-    make_wu_primitive(P)
+    make_wu_primitive(cutoff)
 
 Reproduces the primitive unit cell of "Scheme for Achieving a
 Topological Photonic Crystal by Using Dielectric Material", Wu et al, 2015
+
+Returns a `NamedTuple` of `geometry`, `solver`, `polarisation`, and the
+`G`, `M`, `K` high-symmetry points.
 """
-function make_wu_primitive(P)
+function make_wu_primitive(cutoff)
     # Geometry
     R = 1 / 3 # assuming a0 = 1
     d = 2R/3
@@ -105,30 +136,63 @@ end
 
 
 """
-    make_dePaz(d1::Real, d2::Real, P::Int; R::Real=1/3)
+    make_dePaz(d1::Real, d2::Real, cutoff::Int; R::Real=1/3)
 
 Reproduces "Engineering fragile topology in photonic crystals:
 Topological quantum chemistry of light", Blanco de Paz et al, 2019
+
+Returns a `NamedTuple` of `geometry`, `solver`, `polarisation`, and the
+`G`, `M`, `K` high-symmetry points.
 """
-function make_dePaz(d1::Real, d2::Real, P::Int; R::Real=1/3)
-    return make_wu_generic(R, d1, d2, TM, P)
+function make_dePaz(d1::Real, d2::Real, cutoff::Int; R::Real=1/3)
+    return make_wu_generic(R, d1, d2, TM, cutoff)
 end
-
-function make_dePaz_triv(P::Int)
-    return make_dePaz(0.52, 0.31, P)
-end
-
-function make_dePaz_frag(P::Int)
-    return make_dePaz(0.4, 0.13, P)
-end
-
-function make_dePaz_OAL(P::Int)
-    return make_dePaz(0.4, 0.61, P)
-end
-
 
 """
-    make_zhu(r, l, P; angles=[60n for n in 0:5])
+    make_dePaz_triv(cutoff::Int)
+
+Reproduces the trivial (`d1=0.52`, `d2=0.31`) crystal from "Engineering fragile
+topology in photonic crystals: Topological quantum chemistry of light",
+Blanco de Paz et al, 2019
+
+Returns a `NamedTuple` of `geometry`, `solver`, `polarisation`, and the
+`G`, `M`, `K` high-symmetry points.
+"""
+function make_dePaz_triv(cutoff::Int)
+    return make_dePaz(0.52, 0.31, cutoff)
+end
+
+"""
+    make_dePaz_frag(cutoff::Int)
+
+Reproduces the trivial (`d1=0.4`, `d2=0.13`) crystal from "Engineering fragile
+topology in photonic crystals: Topological quantum chemistry of light",
+Blanco de Paz et al, 2019
+
+Returns a `NamedTuple` of `geometry`, `solver`, `polarisation`, and the
+`G`, `M`, `K` high-symmetry points.
+"""
+function make_dePaz_frag(cutoff::Int)
+    return make_dePaz(0.4, 0.13, cutoff)
+end
+
+"""
+    make_dePaz_OAL(cutoff::Int)
+
+Reproduces the obstructed atomic limit (`d1=0.4`, `d2=0.61`) crystal from
+"Engineering fragile topology in photonic crystals: Topological quantum
+chemistry of light", Blanco de Paz et al, 2019
+
+Returns a `NamedTuple` of `geometry`, `solver`, `polarisation`, and the
+`G`, `M`, `K` high-symmetry points.
+"""
+function make_dePaz_OAL(cutoff::Int)
+    return make_dePaz(0.4, 0.61, cutoff)
+end
+
+
+"""
+    make_zhu(r, l, cutoff; angles=[60n for n in 0:5])
 
 Reproduces "Topological transitions in continuously deformed photonic crystals"
 Zhu et al, 2018.
@@ -137,22 +201,28 @@ Zhu et al, 2018.
 - `l` is the radius of the hexagonal ring that the air holes form
 
 Note that 'l' in Zhu et al is the same as 'R' in wu_ep.
+
+Returns a `NamedTuple` of `geometry`, `solver`, `polarisation`, and the
+`G`, `M`, `K` high-symmetry points.
 """
-function make_zhu(r, l, P; angles=[60n for n in 0:5])
-    return make_wu_generic(l, 2r, 2r, TE, P, ep_cyl=1, ep_bg=10.2)
+function make_zhu(r, l, cutoff; angles=[60n for n in 0:5])
+    return make_wu_generic(l, 2r, 2r, TE, cutoff, ep_cyl=1, ep_bg=10.2)
 end
 
 
 """
-    make_xu(r1, r2, P; wedges=[(0,360)])
+    make_xu(r1, r2, cutoff; wedges=[(0,360)])
 
 Reproduces "Accidental degeneracy in photonic bands and topological phase
 transitions in two-dimensional core-shell dielectric photonic crystals",
 Xu et al, 2016.
 
-The rings can be split into `wedges = [(start,stop), (start,stop)...]`.
+The rings can be split using `wedges = [(start,stop), (start,stop)...]`.
+
+Returns a `NamedTuple` of `geometry`, `solver`, `polarisation`, and the
+`G`, `M`, `K` high-symmetry points.
 """
-function make_xu(r1, r2, P; wedges=[(0,360)])
+function make_xu(r1, r2, cutoff; wedges=[(0,360)])
     function epf(x,y)
         lx, ly = 1, 1*sqrt(3)
         sites = [
@@ -178,7 +248,7 @@ function make_xu(r1, r2, P; wedges=[(0,360)])
     end
     muf(x,y) = 1.0
     geometry = Geometry(epf, muf, -60, +60, 1/500, 1/500)
-    solver = Solver(geometry, P)
+    solver = Solver(geometry, cutoff)
     G = BrillouinZoneCoordinate(  0,   0, "Γ")
     M = BrillouinZoneCoordinate(  0, 1/2, "M")
     K = BrillouinZoneCoordinate(1/3, 1/3, "K")
